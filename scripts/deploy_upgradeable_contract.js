061ca2b267709585f2e4ac1cb6ea3a797100dd35
Aleksei <kilroygood@gmail.com>
2022-06-28 14:50:35 +0300

    add

--- scripts/deploy_upgradeable_contract.js
+++ scripts/deploy_upgradeable_contract.js
@@ -1,17 +1,17 @@
 const { ethers, upgrades } = require('hardhat');
 
 async function main () {
-    // const EtherStore = await ethers.getContractFactory('EtherStore');
-    // console.log('Deploying EtherStore...');
-    // const etherStore = await upgrades.deployProxy(EtherStore);
-    // await etherStore.deployed();
-    // console.log('Box deployed to:', etherStore.address);
+    const EtherStore = await ethers.getContractFactory('EtherStore');
+    console.log('Deploying EtherStore...');
+    const etherStore = await upgrades.deployProxy(EtherStore);
+    await etherStore.deployed();
+    console.log('Box deployed to:', etherStore.address);
 
-    const Attack = await ethers.getContractFactory('Attack');
-    console.log('Deploying attack...');
-    const attack = await upgrades.deployProxy(Attack);
-    await attack.deployed();
-    console.log('attack deployed to:', attack.address);
+    // const Attack = await ethers.getContractFactory('Attack');
+    // console.log('Deploying attack...');
+    // const attack = await upgrades.deployProxy(Attack);
+    // await attack.deployed();
+    // console.log('attack deployed to:', attack.address);
 }
 
 main();
